467365697de6f7096e6ce2e04202887bf8fed9b2
gnostex <gnosticka@gmail.com>
2026-03-28 22:44:01 +1100

    test(db): updated schema.sql -- should pass test success sql execute

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS prices (
     id SERIAL PRIMARY KEY,
-    symbol VARCHAR(10) NOT NULL
+    symbol VARCHAR(10) NOT NULL,
     date DATE NOT NULL,
     open NUMERIC,
     high NUMERIC,
