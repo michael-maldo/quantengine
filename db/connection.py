8127bbcf92ab2606e12118d77fb5c5e66cba1c03
gnostex <gnosticka@gmail.com>
2026-03-30 17:57:02 +1100

    feat(config): implement environment-based configuration for DB (local, CI, production)

--- db/connection.py
+++ db/connection.py
@@ -1,13 +1,23 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+
+def get_env(var):
+    value = os.getenv(var)
+    if not value:
+        raise Exception(f"Missing environment variable: {var}")
+    return value
 
 def get_connection():
     return psycopg2.connect(
-        host="localhost",
-        database="quantengine",
-        user="qe_user",
-        password="testpass",
-        port=5432,
+        host=get_env("DB_HOST"),
+        database=get_env("DB_NAME"),
+        user=get_env("DB_USER"),
+        password=get_env("DB_PASSWORD"),
+        port=get_env("DB_PORT"),
         cursor_factory=RealDictCursor
     )
